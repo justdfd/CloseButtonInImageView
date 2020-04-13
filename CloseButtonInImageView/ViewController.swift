6d428624f357c288aa373702adf5a629c5f91d38
dfd <dfd@justdfd.com>
2020-04-12 21:34:10 -0400

    Initial Commit

---
+++ CloseButtonInImageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloseButtonInImageView
+//
+//  Created by Dave Dombrowski on 4/12/20.
+//  Copyright © 2020 justDFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
